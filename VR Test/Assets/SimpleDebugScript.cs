1564294f1d49b0c7a8e4f594419cbc3648cc410c
Trippy741 <trippy741@gmail.com>
2021-07-13 20:08:30 +0300

    Uploading first demo scene

---
+++ VR Test/Assets/SimpleDebugScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDebugScript : MonoBehaviour
+{
+    public void Log(GameObject gameObject)
+    {
+        Debug.Log("Touched the safety helmet with the: " + gameObject.name + "!");
+    }
+}
