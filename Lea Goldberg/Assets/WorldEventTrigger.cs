8bd76724de14d18eaa4f55ea4ba6e619466fc39e
Trippy741 <trippy741@gmail.com>
2021-07-16 20:26:38 +0300

    new lobby scene

---
+++ Lea Goldberg/Assets/WorldEventTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class WorldEventTrigger : MonoBehaviour
+{
+    public UnityEvent OnTrigger;
+    public Animator triggerAnimator;
+    public string animationTriggerName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(OnTrigger != null)
+            OnTrigger.Invoke();
+        if(triggerAnimator != null)
+            triggerAnimator.SetTrigger(animationTriggerName);
+    }
+}
