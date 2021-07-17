18593ccfcb6130f3db015b00910678793661e1a3
Trippy741 <trippy741@gmail.com>
2021-07-17 19:30:06 +0300

    Update

---
+++ Lea Goldberg/Assets/TypeWriterScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TypeWriterScript : MonoBehaviour
+{
+    [SerializeField] [TextArea] public string[] lines;
+    public TextMeshProUGUI[] textArr;
+    [Range(0,5)]
+    public float delayBtwChar = 2f;
+    public float delayBtwText = 10f;
+    private int textIndex = 0;
+    private int lineIndex = 0;
+    private bool isTyping = false;
+
+    public AudioSource typingAudioSource;
+    public AudioClip[] typingSound;
+
+    private void Start()
+    {
+        foreach (TextMeshProUGUI t in textArr)
+        {
+            t.text = "";
+        }
+    }
+    public void startText()
+    {
+        if (isTyping == true)
+        {
+            //Starting the typeWriterEffect
+            StartCoroutine(TypeText(textArr[lineIndex], lines[lineIndex].ToCharArray()));
+        }
+    }
+    public IEnumerator TypeText(TextMeshProUGUI tmp,char[] arr)
+    {
+        foreach (char c in arr)
+        {
+            tmp.text += c;
+            if(typingAudioSource != null)
+                typingAudioSource.PlayOneShot(typingSound[Random.Range(0,typingSound.Length)]);
+            //A delay between each character
+            yield return new WaitForSeconds(delayBtwChar);
+        }
+        //A delay between each text
+        yield return new WaitForSeconds(delayBtwText);
+        textIndex++;
+        lineIndex++;
+    }
+}
---
+++ Lea Goldberg/Assets/butterflyMomvement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class butterflyMomvement : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    private int currentWaypointIndex = 0;
+    private bool allowMove = false;
+    public void Update()
+    {
+        if(wayPoints[currentWaypointIndex] != null)
+            transform.position += wayPoints[currentWaypointIndex].position;
+    }
+    public void MoveButterFlyToNextPoint()
+    {
+        if (currentWaypointIndex + 1 < wayPoints.Length - 1)
+        {
+            currentWaypointIndex++;
+        }
+    }
+    public void MoveButterFlyToPoint(int pointIndex)
+    {
+        if (pointIndex < wayPoints.Length - 1)
+        {
+            currentWaypointIndex = pointIndex;
+        }
+    }
+    public void MoveButterFlyToPreviousPoint()
+    {
+        if (currentWaypointIndex - 1 < wayPoints.Length - 1)
+        {
+            currentWaypointIndex--;
+        }
+    }
+}
