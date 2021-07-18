9db46f134b42f5bfb2deee528feab6cb33a92232
Trippy741 <trippy741@gmail.com>
2021-07-18 20:57:54 +0300

    Added a main menu scene, implemented scene transition, added vr controller support and the vr controller model

---
+++ Lea Goldberg/Assets/levelChanger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class levelChanger : MonoBehaviour
+{
+    public static levelChanger Instance;
+    public GameObject fadePanel;
+    private Animator animator;
+    private int levelToLoad;
+    private void Awake()
+    {
+        Instance = this;
+        animator = fadePanel.GetComponent<Animator>();
+    }
+    public void FadeToNextLevel()
+    {
+        if (SceneManager.sceneCount >= SceneManager.GetActiveScene().buildIndex + 1)
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            ReplayScene();
+    }
+    public void FadeToPreviousScene()
+    {
+        if (SceneManager.GetSceneAt(SceneManager.GetActiveScene().buildIndex - 1) != null)
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void ReplayScene()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void onFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
