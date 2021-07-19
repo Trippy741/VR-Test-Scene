774570ae4e8b4466713929c5385a5cf59ad25a15
Trippy741 <trippy741@gmail.com>
2021-07-19 16:07:33 +0300

    Finished main menu and added a pause menu, basic controller input system, and a volume slider

---
+++ Lea Goldberg/Assets/PauseMenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuHandler : MonoBehaviour
+{
+    public GameObject PauseMenuObject;
+    private bool paused = false;
+    void Start()
+    {
+        PauseMenuObject.SetActive(false);
+    }
+
+    public void ToggleMenu()
+    {
+        if (paused)
+            PauseMenuObject.SetActive(false);
+        if (!paused)
+            PauseMenuObject.SetActive(true);
+        paused = !paused;
+    }
+}
